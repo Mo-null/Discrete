080124116543df9eeb08576a0740c6128e846067
Mo-null <mhmdkhan10w@gmail.com>
2025-05-19 12:05:16 +0530

    Update 5_Polynomial.py

--- 5_Polynomial.py
+++ 5_Polynomial.py
@@ -1,14 +1,1 @@
 def eval_poly(coeffs, n, power=None):
-    if power is None:
-        power = len(coeffs) - 1  
-    if not coeffs:
-        return 0
-    return coeffs[0] * (n ** power) + eval_poly(coeffs[1:], n, power - 1)
-
-try:
-    coefficients = list(map(int, input("Enter coefficients (highest degree first, space-separated): ").split()))
-    n_value = int(input("Enter value of n: "))
-    result = eval_poly(coefficients, n_value)
-    print("Result:", result)
-except ValueError:
-    print("Invalid input. Please enter integers only.")
